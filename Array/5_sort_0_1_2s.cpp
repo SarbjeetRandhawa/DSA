3dac70e5ee983a4be0ea3aaad044db854e8a7033
Sarbjeet <sarbjeetrandhawa081@gmail.com>
2024-01-09 11:38:49 +0530

    new codes

---
+++ Array/5_sort_0_1_2s.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int arr[5]={0,2,1,0,2};
+   
+
+    for(int i=0; i<5 ; i++){
+        arr2[arr[i]]++;
+    }
+    return 0;
+}
