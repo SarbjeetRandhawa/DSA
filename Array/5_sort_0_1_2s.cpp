1c955921f3af24fc2c4153a3fff7563b20c8b7d0
Sarbjeet <sarbjeetrandhawa081@gmail.com>
2024-01-09 14:00:24 +0530

    new codes

--- Array/5_sort_0_1_2s.cpp
+++ Array/5_sort_0_1_2s.cpp
@@ -1,13 +1,36 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
-int main(int argc, char const *argv[])
-{
-    int arr[5]={0,2,1,0,2};
-   
+void sortArray(vector<int>& arr, int n) {
 
-    for(int i=0; i<5 ; i++){
-        arr2[arr[i]]++;
+    int low = 0, mid = 0, high = n - 1; // 3 pointers
+
+    while (mid <= high) {
+        if (arr[mid] == 0) {
+            swap(arr[low], arr[mid]);
+            low++;
+            mid++;
+        }
+        else if (arr[mid] == 1) {
+            mid++;
+        }
+        else {
+            swap(arr[mid], arr[high]);
+            high--;
+        }
     }
+}
+
+int main()
+{
+    int n = 6;
+    vector<int> arr = {0, 2, 1, 2, 0, 1};
+    sortArray(arr, n);
+    cout << "After sorting:" << endl;
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
     return 0;
 }
